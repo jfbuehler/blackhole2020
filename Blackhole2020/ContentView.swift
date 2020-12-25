414ee9f141b913e5375cc791b33b2452711ad7a1
jfb8618 <jfb8618@gmail.com>
2020-12-24 20:53:06 -0800

    Initial Commit

---
+++ Blackhole2020/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Blackhole2020
+//
+//  Created by Jonathan Buehler on 12/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
