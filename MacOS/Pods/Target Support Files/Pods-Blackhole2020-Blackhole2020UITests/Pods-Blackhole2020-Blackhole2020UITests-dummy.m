546c4f7e81d0fb741bddcf1bd016be396df0643e
Jonathan Buehler <jfb8618@gmail.com>
2022-02-24 19:00:27 -0800

    Remove pods from version control

--- MacOS/Pods/Target Support Files/Pods-Blackhole2020-Blackhole2020UITests/Pods-Blackhole2020-Blackhole2020UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Blackhole2020_Blackhole2020UITests : NSObject
-@end
-@implementation PodsDummy_Pods_Blackhole2020_Blackhole2020UITests
-@end
--- MacOS/Pods/Target Support Files/Pods-Blackhole2020Tests/Pods-Blackhole2020Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Blackhole2020Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Blackhole2020Tests
-@end
